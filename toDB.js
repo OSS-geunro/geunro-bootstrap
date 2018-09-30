44967f83b9e7aaca643fce18c33dfd84f46bd09e
Junhyung Cho <choms46@me.com>
2018-10-01 05:36:59 +0900

    시간표 제출 페이지 제작 및 서버와 기능 연결

---
+++ toDB.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function() {
+  $("#submit").click(function() {
+    $(".alert").hide();
+    var _id = $("#KTISID").val();
+    var _pw = $("#Password").val();
+       if(_id == '' || _pw == ''){
+         alert('아이디와 비밀번호를 입력하세요');
+         return;
+       }
+      
+    var data = {'id' : _id,'pw': _pw}
+    $.ajax({
+      url: "http://0.0.0.0:5009/api/ktis/toDB",
+      type: "post",
+      data: data,
+      success: function(xhr, option, error) {
+        if (xhr.status ==  201) $("#success").show()
+      },
+      error: function(xhr, option, error){
+        if (xhr.status ==  409) $("#warning").show()
+        else if (xhr.status ==  401) $("#danger").show()
+        else alert("...?")
+      },
+    });
+  });
+});
+
+
+
